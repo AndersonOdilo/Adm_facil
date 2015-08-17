e58bd54c4be6bc0ca2f4f82827d4ec9554a0b97e
AndersonOdilo <andersonodilo@hotmail.com>
2015-08-17 20:40:29 -0300

    'assets'

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,10 +1,10 @@
 WickedPdf.config = {
 	if Rails.env.staging? || Rails.env.production?
   	exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-  	layout: "application.pdf.erb",
+  	#layout: "application.pdf.erb",
 	else
   	exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
-    layout: "application.pdf.erb",
+    #layout: "application.pdf.erb",
     #exe_path: 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-end
+	end
 }
